2906ff8c0e524654265ffe2695bc0aaa7d2d8350
EmersonJL <emersonnicolas.jl@gmail.com>
2024-11-04 16:19:17 -0300

    Revision del formulario

--- src/app/components/Contacto/Contacto.js
+++ src/app/components/Contacto/Contacto.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
+import { FormControl, Container, Button } from "@mui/material";
 
 export default function HelperTextAligned() {
   return (
     <Box
-      component="form"
       noValidate
       autoComplete="off"
       sx={{
@@ -18,29 +18,44 @@ export default function HelperTextAligned() {
         "& > :not(style)": { m: 1 },
       }}
     >
-      <TextField
-        helperText="Ingresa tu nombre"
-        id="demo-helper-text-aligned"
-        label="Nombre"
-      />
-      <TextField
-        helperText="Ingresa tu numero de contacto"
-        id="outlined-number"
-        type="number"
-        slotProps={{
-          inputLabel: {
-            shrink: true,
-          },
-        }}
-        label="Contacto"
-      />
-      <TextField
-        helperText="Expresa tu consulta"
-        id="outlined-multiline-static"
-        label="Necesito ..."
-        multiline
-        rows={1}
-      />
+      <Container>
+        <FormControl>
+          <TextField
+            helperText="Ingresa tu nombre"
+            id="demo-helper-text-aligned"
+            label="Nombre"
+            sx={{ marginTop: "10px", marginBottom: "10px" }}
+          />
+          <TextField
+            helperText="Ingresa tu numero de contacto"
+            id="outlined-number"
+            type="number"
+            slotProps={{
+              inputLabel: {
+                shrink: true,
+              },
+            }}
+            label="Contacto"
+            sx={{ marginTop: "10px", marginBottom: "10px" }}
+          />
+          <TextField
+            helperText="Expresa tu consulta"
+            id="outlined-multiline-static"
+            label="Necesito ..."
+            multiline
+            rows={1}
+            sx={{ marginTop: "10px", marginBottom: "10px" }}
+          />
+
+          <Button
+            variant="submit"
+            color="secondary"
+            sx={{ marginTop: "10px", marginBottom: "10px" }}
+          >
+            Enviar
+          </Button>
+        </FormControl>
+      </Container>
     </Box>
   );
 }
