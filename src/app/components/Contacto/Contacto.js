6bcc8b72e6fc679ea658239a2f1058566f8e5cb9
EmersonJL <emersonnicolas.jl@gmail.com>
2024-11-06 18:03:51 -0300

    Metadatos

--- src/app/components/Contacto/Contacto.js
+++ src/app/components/Contacto/Contacto.js
@@ -4,6 +4,12 @@ import TextField from "@mui/material/TextField";
 import { FormControl, Container, Button } from "@mui/material";
 
 export default function HelperTextAligned() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes agregar la lógica para manejar los datos del formulario
+    console.log("Formulario enviado");
+  };
+
   return (
     <Box
       noValidate
@@ -19,42 +25,40 @@ export default function HelperTextAligned() {
       }}
     >
       <Container>
-        <FormControl>
-          <TextField
-            helperText="Ingresa tu nombre"
-            id="demo-helper-text-aligned"
-            label="Nombre"
-            sx={{ marginTop: "10px", marginBottom: "10px" }}
-          />
-          <TextField
-            helperText="Ingresa tu numero de contacto"
-            id="outlined-number"
-            type="number"
-            slotProps={{
-              inputLabel: {
-                shrink: true,
-              },
-            }}
-            label="Contacto"
-            sx={{ marginTop: "10px", marginBottom: "10px" }}
-          />
-          <TextField
-            helperText="Expresa tu consulta"
-            id="outlined-multiline-static"
-            label="Necesito ..."
-            multiline
-            rows={1}
-            sx={{ marginTop: "10px", marginBottom: "10px" }}
-          />
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <TextField
+              helperText="Ingresa tu nombre"
+              id="demo-helper-text-aligned"
+              label="Nombre"
+              sx={{ marginTop: "10px", marginBottom: "10px" }}
+            />
+            <TextField
+              helperText="Ingresa tu numero de contacto"
+              id="outlined-number"
+              type="number"
+              label="Contacto"
+              sx={{ marginTop: "10px", marginBottom: "10px" }}
+            />
+            <TextField
+              helperText="Expresa tu consulta"
+              id="outlined-multiline-static"
+              label="Necesito ..."
+              multiline
+              rows={1}
+              sx={{ marginTop: "10px", marginBottom: "10px" }}
+            />
 
-          <Button
-            variant="submit"
-            color="secondary"
-            sx={{ marginTop: "10px", marginBottom: "10px" }}
-          >
-            Enviar
-          </Button>
-        </FormControl>
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+              sx={{ marginTop: "10px", marginBottom: "10px" }}
+            >
+              Enviar
+            </Button>
+          </FormControl>
+        </form>
       </Container>
     </Box>
   );
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,43 @@
 "use client";
 import React from "react";
+import Head from "next/head";
 
 import "./globals.css";
 
+const metadatos = {
+  title: "Mi Página - Palabra Clave",
+  description: "Descripción de mi página para SEO.",
+  keywords: "palabra clave1, palabra clave2, palabra clave3",
+  robots: "index,follow",
+  ogTitle: "Mi Página - Palabra Clave",
+  ogDescription: "Descripción optimizada para redes sociales.",
+  ogImage: "URL-de-la-imagen",
+  twitterCard: "summary_large_image",
+  twitterTitle: "Mi Página - Palabra Clave",
+  twitterDescription: "Descripción optimizada para Twitter.",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>{metadatos.title}</title>
+        <meta name="description" content={metadatos.description} />
+        <meta name="keywords" content={metadatos.keywords} />
+        <meta name="robots" content={metadatos.robots} />
+        <meta property="og:title" content={metadatos.ogTitle} />
+        <meta property="og:description" content={metadatos.ogDescription} />
+        <meta property="og:image" content={metadatos.ogImage} />
+        <meta property="twitter:card" content={metadatos.twitterCard} />
+        <meta property="twitter:title" content={metadatos.twitterTitle} />
+        <meta
+          property="twitter:description"
+          content={metadatos.twitterDescription}
+        />
+      </Head>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
